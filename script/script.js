4ff5febbbdc13818025276027b04fedc2f926be7
Pires Cerullo <pires.cerullo@icloud.com>
2024-09-12 18:25:02 +0700

    add_project

---
+++ script/script.js
@@ -0,0 +1,4 @@
+function toggleDropdown(dropdownId) {
+    const dropdown = document.getElementById(dropdownId);
+    dropdown.style.display = dropdown.style.display === 'none' ? 'block' : 'none';
+  }
